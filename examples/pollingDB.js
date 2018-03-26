6951a02abcc3c8a9bb759f9fd2fd4a9ac58151b0
anthrogan <30973884+anthrogan@users.noreply.github.com>
2018-03-26 19:33:05 +0100

    check for cpu life
    If .getCpuInfo fails s7client will emit error and stopPollDB() will be called. From there a notification/event can called to notify user/dev

--- examples/pollingDB.js
+++ examples/pollingDB.js
@@ -19,6 +19,7 @@ let dbVars = [
 let pollTimeout;
 async function pollDB() {
   try {
+    const alive = await client.getCpuInfo();
     const plcResult = await client.readDB(dbNr, dbVars);
     console.log(plcResult, "\n");
   } catch (err) {
